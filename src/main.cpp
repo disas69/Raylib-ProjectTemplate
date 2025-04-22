aefd96531e6c265e23a7debaa4d48d59b2a952c8
Eugene <evgenii.esaulenko@gmail.com>
2025-04-22 21:46:07 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <raylib.h>
+
+int main()
+{
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 450;
+
+    const char* title = "Raylib Template";
+    InitWindow(screenWidth, screenHeight, title);
+
+    SetWindowState(FLAG_VSYNC_HINT);
+    SetTargetFPS(60);
+
+    // Load game
+
+    while (!WindowShouldClose())
+    {
+        // Update game
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // Draw game
+
+        EndDrawing();
+    }
+
+    // Unload game
+
+    CloseWindow();
+    return 0;
+}
